b2dcb219c709161df19642fbb64904666ed0bf5f
DAC <dac@cogsmith.com>
2021-04-05 03:48:26 -0400

    .

--- action.js
+++ action.js
@@ -25,7 +25,7 @@ const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 const App = {};
 
-App.Args = { loglevel: 'trace', logfancy: true };
+App.Args = { loglevel: 'info', logfancy: true };
 
 App.LogFancy = false; if (App.Args.logfancy) { App.LogFancy = { colorize: true, singleLine: true, translateTime: 'SYS:yyyy-mm-dd|HH:MM:ss', ignore: 'hostname,pid', messageFormat: function (log, key, label) { let msg = log.msg ? log.msg : ''; let logout = chalk.gray(App.Meta.NameTag); if (msg != '') { logout += ' ' + msg }; return logout; } }; }
 App.Log = pino({ level: App.Args.loglevel, hooks: { logMethod: function (args, method) { if (args.length === 2) { args.reverse() } method.apply(this, args) } }, prettyPrint: App.LogFancy });
