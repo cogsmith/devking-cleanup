a41f4f14da0bf671de559c7e8336252c962e31da
DAC <dac@cogsmith.com>
2021-04-05 14:46:08 -0400

    .

--- action.js
+++ action.js
@@ -83,7 +83,6 @@ App.NukeTags = async function () {
         let cmd = 'git push --delete origin ' + x.name + '';
         cmds.push(cmd);
         LOG.TRACE('NukeTag: ' + cmd);
-        console.log('NukeTag: ' + cmd);
     }
     App.RunCMDS(cmds);
 }
